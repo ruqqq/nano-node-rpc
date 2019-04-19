db5e397e546e652b3921ee0184eee102eab60dc4
Tobias Palmer <tobiaspalmer93@gmail.com>
2019-04-19 13:26:19 +0200

    first ninja api

--- examples_ninja.js
+++ examples_ninja.js
@@ -3,7 +3,7 @@ const NanoClient = require("./lib");
 const NANO_ADDRESS =
   "nano_1ninja7rh37ehfp9utkor5ixmxyg8kme8fnzc4zty145ibch8kf5jwpnzr3r";
 
-const client = new NanoClient({url: 'http://localhost:7076'});
+const client = new NanoClient({apiKey: 'yourapikey'});
 
 const handleErrors = e => {
   console.log("\nError: " + e.message);
@@ -12,11 +12,11 @@ const handleErrors = e => {
 client
   .account_balance(NANO_ADDRESS)
   .then(account => console.log("Account balance:", account))
-  .catch(this.handleErrors);
+  .catch(handleErrors);
 
 client
   .block_count()
   .then(count => console.log("Block count:", count))
-  .catch(this.handleErrors);
+  .catch(handleErrors);
 
 //client.stop().then(() => console.log("Node stopped. Exiting..."));
